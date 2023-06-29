35c2213c5b2ad9fc8c49f6e860e703beca21562d
unknown <lucasaraujoo44.la@gmail.com>
2023-06-29 16:48:17 -0300

    Feat: Recuperando valores das shadow Property

--- Alura.Filmes.App/Dados/AluraFilmesContexto.cs
+++ Alura.Filmes.App/Dados/AluraFilmesContexto.cs
@@ -1,5 +1,6 @@
 ﻿using Alura.Filmes.App.Negocio;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Alura.Filmes.App.Dados
 {
@@ -10,6 +11,31 @@ namespace Alura.Filmes.App.Dados
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=AluraFilmes;Trusted_connection=true;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ator>()
+                 .ToTable("actor");
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.Id)
+                .HasColumnName("actor_id");
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.PrimeiroNome)
+                .HasColumnName("first_name")
+                .HasColumnType("varchar(45)")
+                .IsRequired();
+
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.UltimoNome)
+                .HasColumnName("last_name")
+                .HasColumnType("varchar(45)")
+                .IsRequired();
+            modelBuilder.Entity<Ator>()
+                .Property<DateTime>("last_update")
+                .HasColumnType("datetime")
+                .HasDefaultValueSql("getdate()")
+                .IsRequired();
+                
         }
     }
 }
--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Alura.Filmes.App.Dados;
 using Alura.Filmes.App.Extensions;
+using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 
 namespace Alura.Filmes.App
 {
@@ -7,14 +11,17 @@ namespace Alura.Filmes.App
     {
         static void Main(string[] args)
         {
-            //select * from actor
             using (var contexto = new AluraFilmesContexto())
             {
                 contexto.LogSQLToConsole();
 
-                foreach (var ator in contexto.Atores)
+                var atores = contexto.Atores
+                    .OrderByDescending(a => EF.Property<DateTime>(a, "last_update"))
+                    .Take(10);
+
+                foreach (var ator in atores)
                 {
-                    System.Console.WriteLine(ator);
+                    Console.WriteLine(ator + " - " + contexto.Entry(ator).Property("last_update").CurrentValue);
                 }
             }
         }
